8eaceecf2fc01c85cf95bf0d48362251e50c1c8a
Mitchell-Elbus <elbus3@gmail.com>
2019-05-09 14:48:23 -0400

    messing around with adding a different bootstrap template but still a lot more work to do might have to re-format my folder structure

--- Airline-Reservation/src/app/app.module.ts
+++ Airline-Reservation/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
